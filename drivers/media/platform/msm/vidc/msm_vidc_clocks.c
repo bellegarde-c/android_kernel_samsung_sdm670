ffc0c32826a9abe16b6f693c0b1762b511a7d473
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:50:46 +0900

    msm: vidc: Avoid dynamic memory allocation for small voting data
    Most voting data are rather small and can fit on the stack,
    eliminating the need to allocate them dynamically. Reserve a
    stack buffer for this purpose to avoid the overhead of dynamic
    memory allocation.
    
    Co-authored-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- drivers/media/platform/msm/vidc/msm_vidc_clocks.c
+++ drivers/media/platform/msm/vidc/msm_vidc_clocks.c
@@ -179,7 +179,7 @@ int msm_comm_vote_bus(struct msm_vidc_core *core)
 	int rc = 0, vote_data_count = 0, i = 0;
 	struct hfi_device *hdev;
 	struct msm_vidc_inst *inst = NULL;
-	struct vidc_bus_vote_data *vote_data = NULL;
+	struct vidc_bus_vote_data vote_data[MAX_SUPPORTED_INSTANCES] __aligned(8);
 	bool is_turbo = false;
 
 	if (!core || !core->device) {
@@ -189,20 +189,9 @@ int msm_comm_vote_bus(struct msm_vidc_core *core)
 
 	hdev = core->device;
 
+	memset(vote_data, 0, sizeof(struct vidc_bus_vote_data));
+
 	mutex_lock(&core->lock);
-	list_for_each_entry(inst, &core->instances, list)
-		++vote_data_count;
-
-	vote_data = kcalloc(vote_data_count, sizeof(*vote_data),
-			GFP_TEMPORARY);
-	vote_data_count = 0;
-	if (!vote_data) {
-		dprintk(VIDC_ERR, "%s: failed to allocate memory\n", __func__);
-		mutex_unlock(&core->lock);
-		rc = -ENOMEM;
-		return rc;
-	}
-
 	list_for_each_entry(inst, &core->instances, list) {
 		int codec = 0;
 		struct msm_vidc_buffer *temp, *next;
@@ -313,7 +302,6 @@ int msm_comm_vote_bus(struct msm_vidc_core *core)
 		rc = call_hfi_op(hdev, vote_bus, hdev->hfi_device_data,
 			vote_data, vote_data_count);
 
-	kfree(vote_data);
 	return rc;
 }
 
