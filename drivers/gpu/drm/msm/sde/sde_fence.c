afe3cbf3d0d14f784b54b19741c3995deb18184b
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-26 19:41:22 +0900

    drm/msm/sde: allocate kmem_fence_pool once
    sde_fence_init() is called multiple times without matching
    sde_fence_deinit() calls, resulting in memory leaks and mismatched
    allocs and frees.
    
    Just allocate the kmem pool once.
    
    Fixes: 49b09fc235c8 ("drm/msm/sde: use kmem_cache pool for struct sde_fence")
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_fence.c
+++ drivers/gpu/drm/msm/sde/sde_fence.c
@@ -245,8 +245,6 @@ int sde_fence_init(struct sde_fence_context *ctx,
 	}
 	memset(ctx, 0, sizeof(*ctx));
 
-	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
-
 	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
@@ -267,8 +265,6 @@ void sde_fence_deinit(struct sde_fence_context *ctx)
 	}
 
 	kref_put(&ctx->kref, sde_fence_destroy);
-
-	kmem_cache_destroy(kmem_fence_pool);
 }
 
 void sde_fence_prepare(struct sde_fence_context *ctx)
