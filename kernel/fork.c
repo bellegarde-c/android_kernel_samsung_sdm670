78848a63024dfa8c9cc7311b417cd098003fe1c5
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2021-04-10 07:38:54 +0200

    kernel/fork: Increase boost on app launch
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2113,9 +2113,9 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	/* Boost DDR bus to the max for 500 ms when userspace launches an app */
 	if (task_is_zygote(current))
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
