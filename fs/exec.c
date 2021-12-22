ee67d4dbf37d6a2a0ee3c496969cb0f49f47ef91
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-12-22 12:52:51 +0100

    Do not affine Surfaceflinger and remove CPU restriction for hwcomposer

--- fs/exec.c
+++ fs/exec.c
@@ -1803,13 +1803,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->pc_flags |= PC_PERF_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_perf_mask);
-		} else if (unlikely(!strncmp(filename->name,
-						SURFACEFLINGER_BIN_PREFIX,
-						strlen(SURFACEFLINGER_BIN_PREFIX)))) {
-							current->pc_flags |= PC_PRIME_AFFINE;
-							set_cpus_allowed_ptr(current, cpu_prime_mask);
-						}
+		}
 	}
 
 	/* execve succeeded */
